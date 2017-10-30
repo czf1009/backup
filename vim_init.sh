924f57187167085c356bb81f38373b3866cdfcb3
czf1009 <czf1009@126.com>
2017-10-30 14:43:50 +0800

    Update vim_init.sh

--- vim_init.sh
+++ vim_init.sh
@@ -1,6 +1,5 @@
 cp backup/vimrc_backup ~/.vimrc
 #Set up Vumdle
-if [ -x "~/.vim/bundle/Vundle" ]; then
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 rm backup -rf
